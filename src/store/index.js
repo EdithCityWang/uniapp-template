8e656ce72e26a81f13e142a353590b25ba8ceb12
wangchenghui <wangchenghui526@163.com>
2020-06-30 19:11:29 +0800

    删除多余信息

--- src/store/index.js
+++ src/store/index.js
@@ -7,12 +7,12 @@ export default new Vuex.Store({
 	state: {
 		// 微信配置信息
 		// wxConfig: {
-		// 	"appId": "wxb970a60e95c439ce",
-		// 	"appSecret": "eb1d6214716882795cee5d3edc129dc6",
-		// 	"jsapi_ticket": "kgt8ON7yVITDhtdwci0qeekWcgK20N1oBe1FwwU2btEN030OUj-B7cjA0YU0NADOdBfWM8hMycxIaTSf6lbsSw",
-		// 	"nonceStr": "ew0q915k1ro",
-		// 	"timestamp": "1591580044",
-		// 	"signature": "491f61d09f99c114aa15509e3c4f7fff1b232922"
+		// 	"appId": "",
+		// 	"appSecret": "",
+		// 	"jsapi_ticket": "",
+		// 	"nonceStr": "",
+		// 	"timestamp": "",
+		// 	"signature": ""
 		// },
 		// 用户信息
 		userInfo: {
